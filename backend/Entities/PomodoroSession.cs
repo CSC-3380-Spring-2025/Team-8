ac39cbf7ca89413e9db4e80bf89bb440bbc6723a
queban <queban12@gmail.com>
2025-03-15 17:28:29 -0500

    updated file name and implemented code for pomodoro session

---
+++ backend/Entities/PomodoroSession.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace StudyVerseBackend.Entities;
+
+
+
+public class PomodoroSession
+
+{
+    [Required]
+    public int SessionId { get;set; }
+    
+   [Required] 
+   [Column("user_id")]
+   public int UserId { get; set; }
+   
+   
+   
+   [Required]
+   [Column("finishing_timestamp")]
+   public DateTime FinishingTimeStamp { get; set; }
+
+ 
+   [Required]
+   [MaxLength(255)]
+   [Column("title")]
+   public string Title { get; set; } = string.Empty;
+}
+
+   
