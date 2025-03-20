2bcaa2ef42b30c8fa557bef82f94923e23954c74
Kameron Arceneaux <kameronarceneaux05@gmail.com>
2025-03-20 10:54:23 -0500

    Formally updated the UI package to mantime

--- frontend/src/app/_global_components/Navbar.tsx
+++ frontend/src/app/_global_components/Navbar.tsx
@@ -7,6 +7,7 @@ import { Menu } from "lucide-react";
 const Navbar = () => {
   return (
       <>
+          <p>Hello World</p>
       </>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Navbar from "./_global_components/Navbar";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar/>
-        <div>
-
-        </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        </>
+    );
 }
