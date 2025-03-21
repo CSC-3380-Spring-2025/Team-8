38757e7330e26e595b894efd1d03cdefae1d3d92
Kameron Arceneaux <karce13@lsu.edu>
2025-03-21 13:57:33 -0500

    Initialized base CRUD controller for the Friends table, still need to extend functionality

---
+++ backend/Controllers/FriendsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudyVerseBackend.Entities;
+using StudyVerseBackend.Infastructure.Contexts;
+
+namespace StudyVerseBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FriendsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Friends
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Friends>>> GetFriends()
+        {
+            return await _context.Friends.ToListAsync();
+        }
+
+        // GET: api/Friends/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Friends>> GetFriends(string id)
+        {
+            var friends = await _context.Friends.FindAsync(id);
+
+            if (friends == null)
+            {
+                return NotFound();
+            }
+
+            return friends;
+        }
+
+        // PUT: api/Friends/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFriends(string id, Friends friends)
+        {
+            if (id != friends.RequestorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(friends).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FriendsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Friends
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Friends>> PostFriends(Friends friends)
+        {
+            _context.Friends.Add(friends);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (FriendsExists(friends.RequestorId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetFriends", new { id = friends.RequestorId }, friends);
+        }
+
+        // DELETE: api/Friends/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFriends(string id)
+        {
+            var friends = await _context.Friends.FindAsync(id);
+            if (friends == null)
+            {
+                return NotFound();
+            }
+
+            _context.Friends.Remove(friends);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FriendsExists(string id)
+        {
+            return _context.Friends.Any(e => e.RequestorId == id);
+        }
+    }
+}
