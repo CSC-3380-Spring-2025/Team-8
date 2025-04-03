58e1af2ae771e8df3fbfe618be0fbc1ef708e16e
Kameron Arceneaux <kameronarceneaux05@gmail.com>
2025-04-02 20:04:50 -0500

    Updated navbar to account for quick access tab to friends bar, and added additional navigation. Styling not fully complete.

--- frontend/src/app/_global_components/Navbar.tsx
+++ frontend/src/app/_global_components/Navbar.tsx
@@ -1,18 +1,45 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { Avatar, Badge } from "@mui/material";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import React, {useState, useEffect} from "react";
+import {Avatar, IconButton, Menu, MenuItem} from "@mui/material";
+import GroupIcon from '@mui/icons-material/Group';
+import {useRouter} from "next/navigation";
+import useWindowDimensions from "@/app/custom_hooks/window_dimensions";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+  const {height, width} = useWindowDimensions();
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
     setIsAuthenticated(!!token);
   }, []);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (e: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(e.currentTarget);
+  }
+
+  const handleClose = () => {
+      setAnchorEl(null);
+  }
+
+  const handleSignOut = () => {
+      localStorage.removeItem("authToken");
+
+      if (!localStorage.getItem("authToken")) {
+          router.push("/");
+      }
+  }
+
+  const handleProfileClick = () => {
+      router.push("/profile");
+  }
+
 
     return (
         <nav>
@@ -30,18 +57,9 @@ const Navbar = () => {
                  {isAuthenticated ? (
                     <>
                         <li>
-                            <Link href="/dashboard" className="nav-link">Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link href="/friends" className="nav-link">Friends</Link>
-                        </li>
-                        <li>
-                            <Badge badgeContent={3} color="error">
-                                <NotificationsIcon />
-                            </Badge>
-                        </li>
-                        <li>
-                            <Avatar alt="User Avatar" src="/user-avatar.png" />
+                            <Link href="/friends" className="nav-link">
+                                {width >= 600 ? <GroupIcon/> : "Friends"}
+                            </Link>
                         </li>
                     </>
                     ) : (
@@ -55,6 +73,40 @@ const Navbar = () => {
                     </>
                     )}
             </ul>
+            {isAuthenticated ? (
+                <div id={"userIconNav"}>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <Avatar />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+                        <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+                    </Menu>
+                </div>
+            ) : (
+                <></>
+            )}
         </nav>
     );
 }
