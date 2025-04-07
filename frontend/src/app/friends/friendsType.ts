34e00000125faa9ba8615abcd8b34a17632bf018
Kameron Arceneaux <karce13@lsu.edu>
2025-04-07 15:12:56 -0500

    Setup the Friends tab on the friends page, doesn't contain the actual mockup data

--- frontend/src/app/friends/friendsType.ts
+++ frontend/src/app/friends/friendsType.ts
@@ -1,5 +1,15 @@
 export type UserFriendsRes = {
     id: string,
     name: string,
-    planet: string
+    username: string,
+    planet: string,
+    avatarUrl?: string
 };
+
+export type GalaxyBoostRes = {
+    sender_id: string,
+    sender_name: string,
+    reciever_id: string,
+    message: string,
+    sent_at: string,
+}
--- frontend/src/app/friends/page.tsx
+++ frontend/src/app/friends/page.tsx
@@ -36,11 +36,42 @@ export default function Page() {
     }
 
     const dummyFriends: UserFriendsRes[] = [
-        { id: crypto.randomUUID(), name: 'NovaKnight', planet: 'Mars' },
-        { id: crypto.randomUUID(), name: 'StellarRay', planet: 'Venus' },
-        { id: crypto.randomUUID(), name: 'CometChaser', planet: 'Jupiter' },
+        { id: '1a2b3c4d-0001', name: 'NovaKnight', planet: 'Mars', username: 'nova_knight' },
+        { id: '1a2b3c4d-0002', name: 'StellarRay', planet: 'Venus', username: 'stellar_ray' },
+        { id: '1a2b3c4d-0003', name: 'CometChaser', planet: 'Jupiter', username: 'comet_chaser' },
     ];
 
+
+    const dummyGalaxyBoosts = [
+        {
+            sender_id: '1a2b3c4d-0001',
+            reciever_id: '9z8y7x6w-0001',
+            message: 'Boosted your galaxy!',
+            sent_at: new Date().toString(),
+            sender_name: 'NovaKnight',
+        },
+        {
+            sender_id: '1a2b3c4d-0002',
+            reciever_id: '9z8y7x6w-0002',
+            message: 'Your galaxy is shining!',
+            sent_at: new Date().toString(),
+            sender_name: 'StellarRay',
+        },
+        {
+            sender_id: '1a2b3c4d-0003',
+            reciever_id: '9z8y7x6w-0003',
+            message: 'Your galaxy is shining!',
+            sent_at: new Date().toString(),
+            sender_name: 'CometChaser',
+        },
+        {
+            sender_id: '1a2b3c4d-0003',
+            reciever_id: '9z8y7x6w-0004',
+            message: 'Your galaxy is shining!',
+            sent_at: new Date().toString(),
+            sender_name: 'CometChaser',
+        },
+    ];
 
     return (
         <>
@@ -53,7 +84,7 @@ export default function Page() {
                         <Tab label="Friend Activity"/>
                     </Tabs>
                         <TabPanel value={value} index={0}>
-                            <FriendsStepper friendsInformation={dummyFriends} />
+                            <FriendsStepper friendsInformation={dummyFriends} recentGalaxyBoosts={dummyGalaxyBoosts} />
                         </TabPanel>
                         <TabPanel value={value} index={1}>
                             <h1>Free Real</h1>
