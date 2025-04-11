3f7e1b72ea73c184ea67ac8332598f9e70fed9ec
Kameron Arceneaux <karce13@lsu.edu>
2025-04-11 15:13:12 -0500

    Experimenting with the styling

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,22 +3,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./globals.css";
 import { useState } from "react";
+import StarBackground from "./_global_components/StarBackground";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
+	const [queryClient] = useState(() => new QueryClient());
 
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <html lang="en">
-      <body>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<QueryClientProvider client={queryClient}>
+					{children}
+				</QueryClientProvider>
+			</body>
+		</html>
+	);
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Navbar from "./_global_components/Navbar";
+import StarBackground from "./_global_components/StarBackground";
 
 export default function Home() {
-    return (
-        <>
-            <Navbar/>
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<StarBackground />
+			<Navbar />
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		</>
+	);
 }
