43cbd63c929b1b931192aaa31348fcfed49a7e65
Kameron Arceneaux <karce13@lsu.edu>
2025-04-14 14:00:42 -0500

    Updates to the friends controller

---
+++ backend/Models/Authenticate/UserFriendRes.cs
@@ -0,0 +1,11 @@
+﻿namespace StudyVerseBackend.Models.Authenticate
+{
+    public class UserFriendRes
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Planet { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
