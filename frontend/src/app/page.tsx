ff30dc90d6a00894c0c1bc2b5a1f49be8f142135
Kameron Arceneaux <kameronarceneaux05@gmail.com>
2025-04-20 23:35:23 -0500

    Refactor landing page styles and structure for improved responsiveness

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,3 @@
-
-
 "use client";
 
 import Navbar from "./_global_components/Navbar";
@@ -7,17 +5,19 @@ import StarBackground from "./_global_components/StarBackground";
 import {Container} from "@mui/material";
 
 export default function Home() {
-	return (
-		<>
-			<StarBackground />
-			<Navbar />
-			<Container component="main">
-				<h1>Launch Your Productivity Into Orbit.</h1>
-				<h3> With a fun new way to aim for success  </h3>
-				<div id="container">
-   					 <button id="sign-in-btn">Sign Up</button>
-  			    </div>
-			</Container>
-		</>
-	);
+    return (
+        <>
+            <StarBackground/>
+            <Navbar/>
+            <Container component="main" id={"landing-page"}>
+                <h1>Launch Your Productivity Into Orbit.</h1>
+                <h3> With a fun new way to aim for success </h3>
+                <div id="container">
+                    <button id="sign-in-btn">
+                        <a href="/register">Sign Up</a>
+                    </button>
+                </div>
+            </Container>
+        </>
+    );
 }
